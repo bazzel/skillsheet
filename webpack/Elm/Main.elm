d9a73f9bb45ef57697e2594447e5cba0b4feb4c3
Patrick Baselier <patrick.baselier@gmail.com>
2016-10-20 18:31:05 +0200

    Switch to programWithFlags

--- webpack/Elm/Main.elm
+++ webpack/Elm/Main.elm
@@ -1,7 +1,55 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as App exposing (..)
 
 
+main : Program Flags
 main =
-    text "Hello from Elm!"
+    App.programWithFlags
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
+type alias Flags =
+    { employeesUrl : String }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    let
+        _ =
+            Debug.log "flags" flags
+    in
+        model ! []
+
+
+model : Model
+model =
+    ""
+
+
+type alias Model =
+    String
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    model ! []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    text "Hoi"
--- webpack/application.js
+++ webpack/application.js
@@ -1,3 +1,8 @@
 // inject bundled Elm app into div#main
-var Elm = require('./elm/Main.elm');
-Elm.Main.embed( document.getElementById( 'main' ) );
+var Elm = require('./elm/Main.elm'),
+  hook = document.getElementById('main'),
+  employeesUrl = hook.dataset.employeesUrl,
+  flags = {
+    employeesUrl: employeesUrl
+  };
+Elm.Main.embed(hook, flags);
