cae23f37c1a036b76acad209ecf5fdb8f9d6e3b4
Patrick Baselier <patrick.baselier@gmail.com>
2016-10-20 20:39:34 +0200

    Fetch and render employees

--- webpack/Elm/Main.elm
+++ webpack/Elm/Main.elm
@@ -2,6 +2,10 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as App exposing (..)
+import Task exposing (Task)
+import Http
+import Json.Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (..)
 
 
 main : Program Flags
@@ -20,29 +24,66 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
+    ( initialModel, fetch flags.employeesUrl )
+
+
+initialModel : Model
+initialModel =
+    { employees = [] }
+
+
+fetch : String -> Cmd Msg
+fetch url =
     let
-        _ =
-            Debug.log "flags" flags
+        task =
+            Http.get responseDecoder url
     in
-        model ! []
+        task |> Task.perform HandleResponseError HandleResponseSuccess
 
 
-model : Model
-model =
-    ""
+responseDecoder : Decoder (List Employee)
+responseDecoder =
+    Json.Decode.at [ "employees" ] (Json.Decode.list employeeDecoder)
+
+
+employeeDecoder : Decoder Employee
+employeeDecoder =
+    decode Employee
+        |> required "id" Json.Decode.int
+        |> required "full_name" Json.Decode.string
 
 
 type alias Model =
-    String
+    { employees : List Employee }
+
+
+type alias Employee =
+    { id : Int
+    , name : String
+    }
 
 
 type Msg
-    = NoOp
+    = HandleResponseSuccess (List Employee)
+    | HandleResponseError Http.Error
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model ! []
+    case msg of
+        HandleResponseSuccess employees ->
+            ( { model | employees = employees }, Cmd.none )
+
+        HandleResponseError error ->
+            let
+                _ =
+                    Debug.log "error" error
+            in
+                model ! []
+
+        NoOp ->
+            model ! []
 
 
 subscriptions : Model -> Sub Msg
@@ -52,4 +93,12 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    text "Hoi"
+    div []
+        [ ul []
+            (List.map employeeListItemView model.employees)
+        ]
+
+
+employeeListItemView : Employee -> Html Msg
+employeeListItemView employee =
+    li [] [ text employee.name ]
