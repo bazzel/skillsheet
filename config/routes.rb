b2b43f2592318457cb7113cf1e8ae76c7296fc1a
Patrick Baselier <patrick.baselier@gmail.com>
2016-11-15 18:49:03 +0100

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,25 +5,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'employees/index'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'employees/show'
-    end
-  end
-
-  get 'home/index'
-
-  namespace :api do
-    namespace :v1 do
       resources :employees
     end
   end
-
-  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
