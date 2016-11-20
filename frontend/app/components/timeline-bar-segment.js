669373937671674a1d7baed7d9af9b7136f3b2a1
Patrick Baselier <patrick.baselier@gmail.com>
2016-11-20 15:23:44 +0100

    Timeline

---
+++ frontend/app/components/timeline-bar-segment.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'span',
+  classNames: ['timeline-bar-segment'],
+  attributeBindings: ['style'],
+  style: Ember.computed(function() {
+    let left = this.get('left');
+    let backgroundColor = this.get('backgroundColor');
+
+    return Ember.String.htmlSafe(`${left}${backgroundColor}`);
+  }),
+  left: Ember.computed('experience.startedOn', function() {
+    let start = new Date(this.get('from'), 1, 1);
+    let finish = new Date();
+    let range = finish - start;
+    let startedOn = this.get('experience.startedOn');
+
+    let value = (1 - (finish - startedOn)/range);
+    return `left: ${value*100}%;`;
+  }),
+  backgroundColor: Ember.computed('experience.level', function() {
+    let value;
+    switch (this.get('experience.level')) {
+      case 'Beginner':
+        value = '#E0E6F6';
+        break;
+      case 'Intermediate':
+        value = '#96B3D3';
+        break;
+      case 'Expert':
+        value = '#37628F';
+        break;
+    }
+    return `background-color: ${value};`;
+  })
+});
